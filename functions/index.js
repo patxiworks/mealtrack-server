8e2f66a4ad9ea12c8592417c2a3ab8f3b13ccfe8
patxiworks <patxiworks@gmail.com>
2025-07-04 22:04:20 +0000

    Adjusted onSchedule timer

--- functions/index.js
+++ functions/index.js
@@ -153,6 +153,6 @@ const sendMessagesFromQueue = async (db, admin) => {
     }
 };
 
-exports.checkMealAvailability = onSchedule('*/5 * * * *', async () => { await checkMissingMealAvailability(db, admin); });
-exports.sendMessages = onSchedule('*/5 * * * *', async () => { await sendMessagesFromQueue(db, admin); });
+exports.checkMealAvailability = onSchedule('0 17 * * *', async () => { await checkMissingMealAvailability(db, admin); });
+exports.sendMessages = onSchedule('*/60 * * * *', async () => { await sendMessagesFromQueue(db, admin); });
 exports.subscribe = subscribe;
